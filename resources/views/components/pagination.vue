4dc6ec6e67579c6675b17b3a244d7937c12f053d
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-02 11:07:05 -0600

    feat: Add pagination component

---
+++ resources/views/components/pagination.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
+import { usePaginator } from "momentum-paginator"
+import { ArrowLongLeftIcon, ArrowLongRightIcon } from "@heroicons/vue/24/outline/index.js"
+
+const props = defineProps<{
+  data: Paginator<any>
+}>()
+
+const { pages, previous, next } = usePaginator(props.data)
+</script>
+
+<template>
+  <nav
+    v-if="props.data.data.length"
+    class="-mx-4 mt-4 flex items-center justify-between px-4 sm:mx-0 sm:px-0">
+    <div class="group">
+      <div v-if="previous.isActive" class="-mt-px h-px w-0 transition-[width] group-hover:w-full"></div>
+
+      <Component
+        :is="previous.isActive ? Link : 'span'"
+        :href="previous.url!"
+        class="inline-flex items-center gap-3 py-3 text-sm font-bold transition"
+        :class="[previous.isActive ? 'text-white hover:text-stone-300' : 'cursor-default text-stone-700']">
+        <ArrowLongLeftIcon class="size-5" />
+
+        <span>Previous</span>
+      </Component>
+    </div>
+
+    <div class="hidden md:flex">
+      <div v-for="(page, index) in pages" :key="index">
+        <div v-if="page.isPage" class="group">
+          <div
+            class="-mt-px h-px transition-[width]"
+            :class="[page.isCurrent ? 'w-full' : 'w-0 group-hover:w-full']"></div>
+
+          <Link
+            :href="page.url!"
+            class="block px-4 py-3 text-sm font-bold transition"
+            :class="[page.isCurrent ? 'text-stone-500' : 'text-white']"
+            >{{ page.label }}</Link
+          >
+        </div>
+
+        <div v-else class="px-3.5 py-3 text-sm font-bold ">...</div>
+      </div>
+    </div>
+
+    <div class="group">
+      <div v-if="next.isActive" class="-mt-px h-px w-0 transition-[width] group-hover:w-full"></div>
+
+      <Component
+        :is="next.isActive ? Link : 'span'"
+        :href="next.url!"
+        class="inline-flex items-center gap-3 py-3 text-sm font-bold transition"
+        :class="[next.isActive ? 'text-white hover:text-stone-400' : 'cursor-default text-stone-700']">
+        <span>Next</span>
+
+        <ArrowLongRightIcon class="size-5" />
+      </Component>
+    </div>
+  </nav>
+</template>
