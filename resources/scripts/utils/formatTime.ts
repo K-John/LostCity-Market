be8e7ad5a86b8e527987009895885c6996d38a21
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-02 13:34:37 -0600

    feat: Add formatTime util function

---
+++ resources/scripts/utils/formatTime.ts
@@ -0,0 +1,8 @@
+import dayjs from "dayjs"
+import LocalizedFormat from "dayjs/plugin/localizedFormat"
+
+dayjs.extend(LocalizedFormat)
+
+export const formatTime = (date: string) => {
+  return dayjs(date).format('lll');
+}
