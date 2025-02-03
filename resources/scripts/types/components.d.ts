732f06a70cf9c132e15f947e9415397020babf0a
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-02 19:50:11 -0600

    feat(layout): Update header and footer

--- resources/scripts/types/components.d.ts
+++ resources/scripts/types/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    GithubLogo: typeof import('./../../views/components/github-logo.vue')['default']
     LayoutMain: typeof import('@/views/layouts/main/layout-main.vue')['default']
     Link: typeof import('@inertiajs/vue3')['Link']
     Pagination: typeof import('./../../views/components/pagination.vue')['default']
--- resources/views/layouts/main/layout-main.vue
+++ resources/views/layouts/main/layout-main.vue
@@ -1,4 +1,6 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+    import { HeartIcon } from "@heroicons/vue/24/solid/index.js";
+</script>
 
 <template>
     <div class="mx-auto w-[600px]">
@@ -11,18 +13,32 @@
         </div>
 
         <div class="bg-[url('@/img/assets/background2.jpg')] px-10">
-            <table width="250" bgcolor="black" cellpadding="4" class="mx-auto mb-5">
+            <table
+                width="250"
+                bgcolor="black"
+                cellpadding="4"
+                class="mx-auto mb-5"
+            >
                 <tbody>
                     <tr>
                         <td class="border-2 border-[#382418] leading-tight">
                             <center>
                                 <b>Lost City Markets</b>
-                                
+
                                 <br />
 
-                                <Link href="/" class="text-[#90c040] hover:underline">Main menu</Link>
+                                <Link
+                                    href="/"
+                                    class="text-[#90c040] hover:underline"
+                                    >Main menu</Link
+                                >
 
-                                - <Link :href="route('listings.index')" class="text-[#90c040] hover:underline">My Listings</Link>
+                                -
+                                <Link
+                                    :href="route('listings.index')"
+                                    class="text-[#90c040] hover:underline"
+                                    >My Listings</Link
+                                >
                             </center>
                         </td>
                     </tr>
@@ -30,6 +46,42 @@
             </table>
 
             <slot />
+
+            <div
+                class="mt-4 flex flex-row items-center justify-between gap-4 border-2 border-[#382418] bg-black p-3 text-sm"
+            >
+                <p>
+                    Fan project. Not affiliated with
+                    <a
+                        href="https://2004.lostcity.rs"
+                        target="_blank"
+                        class="text-[#90c040] hover:underline"
+                        >Lost City</a
+                    >
+                </p>
+
+                <div>
+                    <a
+                        href="https://github.com/K-John/LostCity-Market"
+                        target="_blank"
+                        class="flex justify-end gap-2 text-[#90c040] hover:underline"
+                        ><GithubLogo class="size-5" /> Source Code</a
+                    >
+
+                    <p class="flex gap-1">
+                        Made by
+                        <a
+                            href="https://github.com/K-John"
+                            target="_blank"
+                            class="text-[#90c040] hover:underline"
+                        >
+                            BigShot
+                        </a>
+                        with love
+                        <HeartIcon class="size-5 text-red-500" />
+                    </p>
+                </div>
+            </div>
         </div>
 
         <div class="flex items-end">
