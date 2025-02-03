8e6b0d3b6f7d8f2c01cca3a303792c1e4072017f
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-02 19:05:59 -0600

    feat: Shortened timestamp text

--- resources/scripts/utils/fromNow.ts
+++ resources/scripts/utils/fromNow.ts
@@ -1,11 +1,31 @@
-import dayjs from "dayjs"
-import relativeTime from "dayjs/plugin/relativeTime"
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "%s",
+    past: "%s",
+    s: "few secs",
+    m: "1 min",
+    mm: "%d mins",
+    h: "1 hr",
+    hh: "%d hrs",
+    d: "1 day",
+    dd: "%d days",
+    M: "1 mo",
+    MM: "%d mo",
+    y: "1 yr",
+    yy: "%d yr",
+  },
+});
 
 /**
- * Convert a date to a relative time
+ * Convert a date to a shortened relative time format
  */
 export const fromNow = (date: string) => {
-  return dayjs(date).fromNow()
-}
+  return dayjs(date).fromNow();
+};
