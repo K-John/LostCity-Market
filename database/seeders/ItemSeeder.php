82cf67d97d4bf503e57a56990e699e8a72896b79
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-03 00:22:50 -0600

    feat: Update seeder to use items from obj.json

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -12,29 +12,15 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        $filePath = storage_path('app/private/obj.pack');
-        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $items = json_decode(file_get_contents(storage_path('app/private/obj.json')), true);
+        $itemIds = array_column($items, 'id');
 
-        $items = array_map(function ($line) {
-            [$id, $slug] = explode('=', $line);
-            $name = ucwords(str_replace('_', ' ', trim($slug)));
-
-            return [
-                'game_id' => $id,
-                'slug' => trim($slug),
-                'name' => $name,
-            ];
-        }, $lines);
-
-        // Filter out items where slug contains "cert"
-        $items = array_filter($items, function ($item) {
-            return strpos($item['slug'], 'cert') === false;
-        });
-
-        // Re-index the array to prevent gaps
-        $items = array_values($items);
-
-        // Find any items in $items array that have a matching id, slug, or name
-        DB::table('items')->insert($items);
+        DB::table('items')->whereNotIn('game_id', $itemIds)->delete();
+        foreach ($items as $item) {
+            DB::table('items')->updateOrInsert(
+                ['game_id' => $item['id']],
+                ['slug' => $item['debugname'], 'name' => $item['name']]
+            );
+        }
     }
 }
