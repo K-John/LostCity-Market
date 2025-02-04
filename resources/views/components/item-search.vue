bfe654ac535ae79464ae97438e227cbf387d908d
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-04 14:33:20 -0600

    refactor: Fix slightly off-center title

--- resources/views/components/item-search.vue
+++ resources/views/components/item-search.vue
@@ -8,7 +8,7 @@ const navigateToItem = (item: Data.Item.ItemData) => {
     <div
         class="mb-4 flex flex-row gap-4 border-2 border-[#382418] bg-black p-3"
     >
-        <h2 class="text-lg font-bold">Search for an item:</h2>
+        <h2 class="flex items-center text-lg font-bold">Search for an item:</h2>
 
         <ItemSelect @item-selected="navigateToItem" />
     </div>
