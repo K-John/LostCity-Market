9d2f015188434e24298b3b8761c972350df2ade8
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-05 17:15:17 -0600

    fix: fromNow timestamp not updating live

--- resources/views/components/listings-table/timestamp-table-data.vue
+++ resources/views/components/listings-table/timestamp-table-data.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 import { Tooltip } from "floating-vue";
 import "floating-vue/dist/style.css";
 
-defineProps<{
+const props = defineProps<{
     timestamp: string;
 }>();
+
+const timeAgo = ref(fromNow(props.timestamp));
+let interval: ReturnType<typeof setInterval> | null = null;
+
+onMounted(() => {
+    interval = setInterval(() => {
+        timeAgo.value = fromNow(props.timestamp);
+    }, 60000); // 60,000ms = 1 minute
+});
+
+onUnmounted(() => {
+    if (interval) clearInterval(interval);
+});
 </script>
 
 <template>
     <td class="px-1">
         <Tooltip>
-            <p>{{ fromNow(timestamp) }}</p>
+            <p>{{ timeAgo }}</p>
 
             <template #popper>
                 {{ formatTime(timestamp) }}
