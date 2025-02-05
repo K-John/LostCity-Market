3f3fa3cddbceb36c549036e6fb5390a7696475ce
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-04 23:40:02 -0600

    refactor: Update home to listing components

--- resources/views/pages/home/index/page.vue
+++ resources/views/pages/home/index/page.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import { Tooltip } from "floating-vue";
-import "floating-vue/dist/style.css";
-import {
-    PencilSquareIcon,
-    TrashIcon,
-} from "@heroicons/vue/24/outline/index.js";
 import { usePoll } from "@inertiajs/vue3";
 
 const props = defineProps<Pages.HomeIndexPage>();
 const listingTypes = computed((): Enums.ListingType[] => ["buy", "sell"]);
 
-const destroy = (id: number) => {
-    router.delete(route("listings.destroy", { listing: id }), {
-        preserveScroll: true,
-    });
-};
 usePoll(5000);
 
 const mostRecentUpdateDate = computed(() => {
@@ -66,135 +55,31 @@ const highlightedIds = ref<number[]>([]);
             </Link>
         </div>
 
-        <div class="flex flex-col border-2 border-[#382418] bg-black">
-            <table class="border-separate border-spacing-2">
-                <tbody>
-                    <tr v-if="!listings.data.length">
-                        <td class="text-center" colspan="4">
-                            No listings found.
-                        </td>
-                    </tr>
+        <ListingTable @mouseenter="highlightedIds = []">
+            <EmptyTableRow v-if="!props.listings.data.length" />
 
-                    <tr v-for="listing in listings.data" :key="listing.id">
-                        <td>
-                            <Tooltip>
-                                <Link
-                                    v-if="listing.item"
-                                    :href="
-                                        route('items.show', {
-                                            item: listing.item.slug,
-                                        })
-                                    "
-                                >
-                                    <img
-                                        :src="`/img/items/${listing.item.slug}.png`"
-                                        :alt="`${listing.item.name} Icon`"
-                                        width="32"
-                                        height="32"
-                                    />
-                                </Link>
+            <ListingTableRow
+                v-for="l in listings.data"
+                :key="l.id"
+                :listing="l"
+                :highlighted="highlightedIds.includes(l.id)"
+            >
+                <template #default="{ listing }">
+                    <ItemTableData :item="listing.item" />
 
-                                <template #popper>
-                                    {{
-                                        listing.item
-                                            ? listing.item.name
-                                            : "Unknown Item"
-                                    }}
-                                </template>
-                            </Tooltip>
-                        </td>
+                    <PriceTableData :listing="listing" />
 
-                        <td class="flex gap-1">
-                            <span
-                                :class="
-                                    listing.type === 'buy'
-                                        ? 'text-red-500'
-                                        : 'text-green-500'
-                                "
-                                class="font-bold"
-                            >
-                                [{{ listing.type.charAt(0).toUpperCase() }}]
-                            </span>
+                    <UsernameTableData :username="listing.username" />
 
-                            <Tooltip>
-                                <p>{{ formatGold(listing.quantity) }}</p>
+                    <TimestampTableData :timestamp="listing.updatedAt" />
+                    
+                    <NoteTableData :listing="listing" />
+                </template>
+            </ListingTableRow>
 
-                                <template #popper>
-                                    {{ listing.quantity.toLocaleString() }}
-                                </template>
-                            </Tooltip>
-
-                            for
-
-                            <Tooltip>
-                                <p>{{ formatGold(listing.price) }}GP ea.</p>
-
-                                <template #popper>
-                                    {{ listing.price.toLocaleString() }}
-                                </template>
-                            </Tooltip>
-                        </td>
-
-                        <td class="text-stone-400">
-                            {{
-                                listing.username
-                                    .split(" ")
-                                    .map(
-                                        (word) =>
-                                            word.charAt(0).toUpperCase() +
-                                            word.slice(1).toLowerCase(),
-                                    )
-                                    .join(" ")
-                            }}
-                        </td>
-
-                        <td>
-                            <Tooltip>
-                                <p>{{ fromNow(listing.updatedAt) }}</p>
-
-                                <template #popper>
-                                    {{ formatTime(listing.updatedAt) }}
-                                </template>
-                            </Tooltip>
-                        </td>
-
-                        <td class="max-w-[110px]">
-                            <div
-                                v-if="listing.canManage"
-                                class="flex flex-nowrap justify-end gap-1"
-                            >
-                                <Link
-                                    :href="route('listings.edit', { listing })"
-                                    class="w-fit rounded-md bg-amber-300 px-2 py-1 text-amber-900 hover:bg-amber-400"
-                                >
-                                    <PencilSquareIcon class="size-5" />
-                                </Link>
-
-                                <button
-                                    class="w-fit rounded-md bg-red-300 px-2 py-1 text-red-900 hover:bg-red-400"
-                                    @click="destroy(listing.id)"
-                                >
-                                    <TrashIcon class="size-5" />
-                                </button>
-                            </div>
-
-                            <template v-else>
-                                <Tooltip>
-                                    <p class="truncate">{{ listing.notes }}</p>
-
-                                    <template #popper>
-                                        {{ listing.notes }}
-                                    </template>
-                                </Tooltip>
-                            </template>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <div v-if="listings.data.length" class="px-3">
+            <template v-if="listings.data.length" #footer>
                 <Pagination :data="listings" />
-            </div>
-        </div>
+            </template>
+        </ListingTable>
     </LayoutMain>
 </template>
