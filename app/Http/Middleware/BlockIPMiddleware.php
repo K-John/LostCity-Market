d53bb1116a41faf5cde15536a2b6f501571bce1e
Kendall Johnson <kenjohnson1994@gmail.com>
2025-02-11 13:09:40 -0600

    chore: Avoid checking blocked ip every request

--- app/Http/Middleware/BlockIPMiddleware.php
+++ app/Http/Middleware/BlockIPMiddleware.php
@@ -9,13 +9,18 @@ class BlockIPMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        $blockedIps = explode(',', env('BLOCKED_IPS', ''));
+        if ($request->session()->has('ip_checked')) {
+            return $next($request);
+        }
 
+        $blockedIps = explode(',', env('BLOCKED_IPS', ''));
         $blockedIps = array_map('trim', $blockedIps);
 
         if (in_array($request->ip(), $blockedIps)) {
             return response()->json(['message' => 'Access Denied.'], 403);
         }
+
+        $request->session()->put('ip_checked', true);
 
         return $next($request);
     }
