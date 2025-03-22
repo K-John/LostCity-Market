125073eb8ff043ea005cf6be5cc491410b0146d0
BigShot <kenjohnson1994@gmail.com>
2025-03-22 15:38:34 -0500

    feat: Add favorites migration and relationship

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Item extends Model
@@ -16,5 +17,11 @@ class Item extends Model
     public function listings(): HasMany
     {
         return $this->hasMany(Listing::class);
+    }
+
+    public function favoritedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withTimestamps();
     }
 }
