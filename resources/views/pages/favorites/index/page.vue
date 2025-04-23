41c913f0d819009608ec90e696982613b794260a
BigShot <kenjohnson1994@gmail.com>
2025-04-22 19:18:23 -0500

    feat(favorites): Update styling and add create form

--- resources/views/pages/favorites/index/page.vue
+++ resources/views/pages/favorites/index/page.vue
@@ -1,23 +1,55 @@
 <script setup lang="ts">
-import { XMarkIcon } from "@heroicons/vue/24/outline";
+import { InformationCircleIcon, PlusIcon } from "@heroicons/vue/24/outline";
 const props = defineProps<Pages.FavoritesIndexPage>();
+
+const selectedItem = ref<Data.Item.ItemData | null>(null);
+
+watch(selectedItem, (item) => {
+    if (item) {
+        router.post(
+            route("favorites.store", {
+                item_id: item.id,
+                preserveScroll: true,
+            }),
+        );
+        selectedItem.value = null;
+    }
+});
 </script>
 
 <template>
     <LayoutAccount>
-        <Head title="My Favorites" />
+        <Head title="Favorite Items" />
 
-        <ListingTable>
-            <template #header>
-                <h1 class="text-lg font-bold">My Favorites:</h1>
-            </template>
+        <h1 class="mb-4 text-2xl font-semibold">Favorite Items</h1>
 
+        <Alert type="info">
+            <div class="flex items-center gap-2">
+                <InformationCircleIcon class="size-6 text-sky-200" />
+
+                <p>
+                    View listings of your favorite items by navigating to the
+                    <Link
+                        href="/?tab=favorites"
+                        class="text-[#90c040] hover:underline"
+                        >Favorites tab in the Main Menu</Link
+                    >.
+                </p>
+            </div>
+        </Alert>
+
+        <ListingTable class="pt-2">
             <tr v-for="item in props.items.data" :key="item.id">
                 <ItemTableData :item="item" />
 
                 <td>
-                    <button
-                        class="text-red-500 hover:text-red-700"
+                    {{ item.name }}
+                </td>
+
+                <td>
+                    <BaseButton
+                        as="button"
+                        variant="danger"
                         @click="
                             router.delete(
                                 route('favorites.destroy', {
@@ -28,7 +60,7 @@ const props = defineProps<Pages.FavoritesIndexPage>();
                         "
                     >
                         Remove
-                    </button>
+                    </BaseButton>
                 </td>
             </tr>
 
@@ -40,5 +72,31 @@ const props = defineProps<Pages.FavoritesIndexPage>();
                 <Pagination :data="items" />
             </template>
         </ListingTable>
+
+        <hr class="mb-5 mt-6 border-t-2 border-stone-700" />
+
+        <div class="mb-4 flex gap-2">
+            <PlusIcon class="size-7 text-green-400" />
+
+            <div>
+                <h2 class="text-xl font-semibold leading-none">
+                    Add Favorite Items
+                </h2>
+
+                <p class="text-stone-300">
+                    You can add favorite items here or directly from an item's page.
+                </p>
+            </div>
+        </div>
+
+        <Alert class="mb-6">
+            <div class="flex flex-col gap-4 sm:flex-row">
+                <h3 class="flex items-center text-lg font-bold">
+                    Search for an item:
+                </h3>
+
+                <ItemSelect v-model="selectedItem" />
+            </div>
+        </Alert>
     </LayoutAccount>
 </template>
