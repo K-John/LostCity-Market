aceaea19f086ce462e21166df0faee5e70c4b9ed
BigShot <kenjohnson1994@gmail.com>
2025-05-01 19:47:54 -0500

    feat: Improve formatGold to be more precise

--- resources/scripts/utils/formatGold.ts
+++ resources/scripts/utils/formatGold.ts
@@ -1,10 +1,18 @@
 export default function formatGold(value: number): string {
+    const truncate = (num: number): number => Math.floor(num * 100) / 100;
+
+    const format = (val: number, suffix: string): string => {
+        const truncated = truncate(val);
+        return `${truncated % 1 === 0 ? truncated.toFixed(0) : truncated}${suffix}`;
+    };
+
     if (value >= 1_000_000) {
-        return (
-            (value / 1_000_000).toFixed(value % 1_000_000 === 0 ? 0 : 1) + "M"
-        );
-    } else if (value >= 1_000) {
-        return (value / 1_000).toFixed(value % 1_000 === 0 ? 0 : 1) + "K";
+        return format(value / 1_000_000, "M");
     }
+
+    if (value >= 1_000) {
+        return format(value / 1_000, "K");
+    }
+
     return value.toString();
 }
