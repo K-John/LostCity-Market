374d8eb8fec3b6cfaa01466e2feabc6d2fede179
BigShot <kenjohnson1994@gmail.com>
2025-05-03 20:48:35 -0500

    fix: Gold formatting issues

--- resources/scripts/utils/formatGold.ts
+++ resources/scripts/utils/formatGold.ts
@@ -1,18 +1,11 @@
 export default function formatGold(value: number): string {
-    const truncate = (num: number): number => Math.floor(num * 100) / 100;
-
-    const format = (val: number, suffix: string): string => {
-        const truncated = truncate(val);
-        return `${truncated % 1 === 0 ? truncated.toFixed(0) : truncated}${suffix}`;
-    };
-
     if (value >= 1_000_000) {
-        return format(value / 1_000_000, "M");
+        return `${Number((value / 1_000_000).toFixed(2)).toLocaleString()}M`;
     }
 
     if (value >= 1_000) {
-        return format(value / 1_000, "K");
+        return `${Number((value / 1_000).toFixed(2)).toLocaleString()}K`;
     }
 
-    return value.toString();
+    return value.toLocaleString();
 }
